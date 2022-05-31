731085f6fb513f002faebc5723bb61c5361d61bf
Alejandro <unclearcoder@hotmail.com>
2022-05-31 10:01:34 -0400

    function for adding time
    Counted for change in AM to PM

--- time-calculator.py
+++ time-calculator.py
@@ -1,18 +1,55 @@
 # Time function
 def add_time(time, add):
-    # convert time into minutes
+    # extract the data and check for errors
+    suffix = None
     time = time.split()
+    if time[1].upper() == 'PM':
+        suffix = 2
+    if time[1].upper() == 'AM':
+        suffix = 1
+    if type(suffix) != int:
+        print('Error: please specify if it\'s "AM" or "PM"')
+        quit()
+
+    # check for valid time and change into int
     time = time[0].split(':')
-    time = int(time[0]) * 60 + int(time[1])
-    # convert add time into minutes
+    for index, value in enumerate(time):
+        try:
+            time[index] = int(value)
+        except ValueError:
+            print('Error: please specify a valid time')
+            quit()
+    # convert hour to minutes
+    time = time[0] * 60 + time[1]
+
+    # convert add time into minutes and check for errors
     add = add.split()
     add = add[0].split(':')
+    for index, value in enumerate(add):
+        try:
+            add[index] = int(value)
+        except ValueError:
+            print('Error: please specify a valid time')
+            quit()
     add = int(add[0]) * 60 + int(add[1])
     # new time
-    hour = (time + add) // 60
-    minutes = (time + add) % 60
-    solution = f'{hour}:{minutes}'
+    unit = time + add
+    hour = unit // 60
+    minutes = unit % 60
+
+    # change from minutes to hour
+    if hour > 1:
+        suffix = suffix + hour // 12
+        if hour == 0:
+            hour = 12
+        hour = hour - (hour // 12) * 12
+
+    if suffix % 2 == 0:
+        suffix = 'PM'
+    else:
+        suffix = 'AM'
+    solution = f'{hour}'.zfill(2) + ':' + f'{minutes}'.zfill(2) + f' {suffix}'
     return solution
 
 
-print(add_time("3:00 PM", '3:10'))
+print(add_time("12:50 pm", '25:30'))
